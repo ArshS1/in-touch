183f23d47014eaf19b1c027c36fd34fb8d5c314b
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2022-10-30 16:48:15 -0400

    finish header -- finished all sides

--- components/Header.js
+++ components/Header.js
@@ -8,12 +8,17 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
+// component for the header of the website
+// included all 3 sides (search bar, logo, profile icon)
+
+// todo: center search icon
 
 function Header() {
   return (
     <div className="">
-      <div className="flex justify-between max-w-6xl">
-        {/* LEFT SECTION */}
+      <div className="flex justify-between items-center max-w-6xl mx-5 xl:mx-auto">
+        {/* LEFT SECTION aka LOGO */}
         <div className="relative hidden lg:inline-grid w-24 h-24">
           <Image
             src="https://www.logo.wine/a/logo/Instagram/Instagram-Wordmark-Logo.wine.svg"
@@ -23,7 +28,7 @@ function Header() {
           />
         </div>
 
-        <div className="relative w-20 h-20 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="https://i.ibb.co/w7m7ssc/hands-removebg-preview.png"
             layout="fill"
@@ -32,17 +37,39 @@ function Header() {
         </div>
         {/* LEFT SECTION ENDS */}
 
-        {/* MIDDLE SECTION */}
+        {/* MIDDLE SECTION aka SEARCH */}
         <div>
-          <div></div>
-          <input type="text" placeholder="Search" />
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute pl-3 flex inset-y-0 items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black"
+            />
+          </div>
         </div>
         {/* MIDDLE SECTION ENDS */}
 
-        {/* RIGHT SECTION */}
-      </div>
+        {/* RIGHT SECTION aka PROFILE PIC */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <PaperAirplaneIcon className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
 
-      {/* RIGHT SECTION ENDS */}
+          <img
+            src="https://www.arshdeeps.com/_next/static/media/navIcon.4190af36.svg"
+            alt="Profile Pic"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
+
+        {/* RIGHT SECTION ENDS */}
+      </div>
     </div>
   );
 }
