e8cbbc81bf084d8106ba946d9e2a6cee9dda6f9b
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2022-11-27 16:27:22 -0500

    setup global state + finish modal component

--- components/Header.js
+++ components/Header.js
@@ -12,11 +12,14 @@ import {
 import { HomeIcon } from "@heroicons/react/solid";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modalAtom";
 // component for the header of the website
 // included all 3 sides (search bar, logo, profile icon)
 
 function Header() {
   const { data: session, status } = useSession();
+  const [open, setOpen] = useRecoilState(modalState);
   const router = useRouter();
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50">
@@ -68,7 +71,10 @@ function Header() {
           {session ? (
             <>
               <PaperAirplaneIcon className="navBtn" />
-              <PlusCircleIcon className="navBtn" />
+              <PlusCircleIcon
+                onClick={() => setOpen(true)}
+                className="navBtn"
+              />
               <UserGroupIcon className="navBtn" />
               <HeartIcon className="navBtn" />
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import Header from "../components/Header"
-import Feed from "../components/Feed"
+import Header from "../components/Header";
+import Feed from "../components/Feed";
+import Modal from "../components/Modal";
 
 const Home: NextPage = () => {
   return (
@@ -22,7 +23,7 @@ const Home: NextPage = () => {
       {/* FEED ENDS */}
 
       {/* MODAL */}
-
+      <Modal />
       {/* MODAL ENDS */}
     </div>
   );
